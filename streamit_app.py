bac6f87c27b02db658a104dc765c96ea32efd1c5
MMRockborne <martamoreira@rockborne.com>
2024-08-08 10:48:51 +0100

    Update streamit_app.py
    Corrected the code

--- streamit_app.py
+++ streamit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
@@ -9,15 +10,19 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
 
-session = get_active_session()
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    my_dataframe
+    my_dataframe,
+    max_selections=5
 )
 
 if ingredients_list:
@@ -29,9 +34,10 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+            values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
+    #st.stop()
 
     time_to_insert = st.button('Submit Order'
                               )
